f179021afe05a56a2536cc8a24999a0df9497240
giuliacrr <giulia.corrado02@gmail.com>
2023-07-06 19:54:39 +0200

    Added exercise commet in the js sheet.

--- Snack2/js/main.js
+++ Snack2/js/main.js
@@ -1,6 +1,7 @@
+//Snack2
+//Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let sum = 0;
-
 for (let x = 0; x < numbers.length; x++) {
   //console.log(numbers[x]);
   if (numbers[x] % 2 !== 0) {
@@ -8,6 +9,5 @@ for (let x = 0; x < numbers.length; x++) {
     //Stampa la lista dei numeri con indice dispari
     sum = sum + numbers[x];
   }
-
 }
 console.log("This is the sum of the odd index elements:" + " " + sum);
