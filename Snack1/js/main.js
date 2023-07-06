4d991aaf3d27258d5774b715803c1f78bc079816
giuliacrr <giulia.corrado02@gmail.com>
2023-07-06 16:24:55 +0200

    Html part of guests list

--- Snack1/js/main.js
+++ Snack1/js/main.js
@@ -4,7 +4,7 @@
 //Surnames           0        1        2        3       4        5         6           7            8            9          10
 const surnames = ["Leone", "Linza", "Bono", "Pazi", "Glorian", "Giuri", "Collura", "Carluccio", "Filograna", "Isabelli", "Porrega"];
 //Names           0        1           2            3           4          5         6          7        8           9          10
-const names = ["Padre", "Corrado", "Pasquale", "Salvatore", "Giulia", "Pietro", "Riccardo", "Elisa", "Matteo", "Vittoria", "Paola"];
+const names = ["Padre", "Corrado", "Pasquale", "Salvatore", "Giulia", "Pietro", "Riccardo", "Elisa", "Matteo", "Michele", "Paola"];
 
 console.log(surnames);
 console.log(names);
@@ -14,14 +14,20 @@ const rngNames = Math.floor(Math.random() * names.length);
 //rng Name and Surnames console print
 console.log(rngSurnames);
 console.log(rngNames);
-
 // Guest list
-for (let x = 0; x < 20; x++) {
+for (let x = 0; x < 11; x++) {
   const rngSurnames = Math.floor(Math.random() * surnames.length);
   const rngNames = Math.floor(Math.random() * names.length);
   //rng Name and Surnames console print
-  console.log(names[rngNames] + " " + surnames[rngSurnames]);
+  const list = names[rngNames] + " " + surnames[rngSurnames];
+  const listContainer = document.getElementById("list");
+  console.log(list);
+  //Print Guest list
+  let singleGuest = document.createElement("div");
+  singleGuest.classList.add("single-guest", "text-center");
+  //singleGuest.innerHtml = list.toString();
+  singleGuest.append(list);
+  listContainer.append(singleGuest);
   //Come faccio a far uscire tutti nomi diversi??
-
 }
 
